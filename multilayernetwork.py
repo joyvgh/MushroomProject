34fc6e0ce510311bb89eaaf9c773ff52d10ff2b0
Valerie Lambert <lambertv@carleton.edu>
2016-05-26 18:50:45 -0500

    everything should work now

--- multilayernetwork.py
+++ multilayernetwork.py
@@ -15,8 +15,6 @@ def logistic(x, T):
 def dlogistic(x, T):
     """Derivative of the logistic function. Note that x has
     had the logistic function already applied to it."""
-    #look here
-    #ask Anna
     return T * x*(1-x)
 
 def propagate(x, w, b, f, T):
@@ -36,8 +34,8 @@ def derror(target, y):
     """Derivative of the error function."""
     return target-y
 
-def our_error(target, computed, weights, lambda1, lambda2):
-    err = np.sum(error(target, computed)) * .5
+def our_error(weights, lambda1, lambda2):
+    err = 0
 
     weight_sum = 0
     for layer in weights:
@@ -46,8 +44,7 @@ def our_error(target, computed, weights, lambda1, lambda2):
             j = (weight - 1) ** 2
             k = (weight + 1) ** 2
             weight_sum += lambda2 * i * j * k + lambda1 * weight**2
-    weight_sum = weight_sum 
-    err += weight_sum
+    err += weight_sum*0.5
 
     return err
 
@@ -58,8 +55,6 @@ def our_derror(target, computed, weights, lambda1, lambda2):
         for weight in layer[0]:
             weight_sum += lambda1*weight + lambda2*(weight**2 - 1)*(3*weight**2 - 1)
     return np.add(d_error, weight_sum)
-
-
 
 def update_weights(x, y, weights, bias, eta, T, lambda1, lambda2):
     """Update the weights of a 2-layer neural network, given the
@@ -74,21 +69,33 @@ def update_weights(x, y, weights, bias, eta, T, lambda1, lambda2):
     (w0, w1) = weights
     (b0, b1) = bias
 
+    w0 += lambda1 * w0 + lambda2 * w0 * (w0**2 - 1) * (3 * w0**2 - 1)
+    w1 += lambda1 * w1 + lambda2 * w1 * (w1**2 - 1) * (3 * w1**2 - 1)
+
+    err = 0
+
     for i in range(x.shape[1]):
         # Propagate through the network
         x0 = x[:, [i]]
         x1 = propagate(x0, w0, b0, logistic, T)
         x2 = propagate(x1, w1, b1, logistic, T)
 
-        # compute weight delta for the second layer
-        d1 = our_derror(y[:, [i]], x2, weights, lambda1, lambda2) * dlogistic(x2, T)
+        err += np.sum(error(y[:, [i]], x2))
 
-        dw1 = eta * np.dot(d1, x1.T)
+        # compute weight delta for the second layer
+        d1 = derror(y[:, [i]], x2) * dlogistic(x2, T)
+
+        a = np.dot(d1, x1.T)
+        b = a + w1*lambda1
+
+        dw1 = eta * b
         db1 = eta * d1.sum(axis=1)
 
         # compute weight delta for first layer
         d0 = np.dot(d1.T, w1).T * dlogistic(x1, T)
-        dw0 = eta * np.dot(d0, x0.T)
+        a = np.dot(d0, x0.T)
+        b = a + w0*lambda1
+        dw0 = eta * b
         db0 = eta * d0.sum(axis=1)
 
         # update weights
@@ -99,8 +106,10 @@ def update_weights(x, y, weights, bias, eta, T, lambda1, lambda2):
         b0 += db0
         b1 += db1
 #look here
+        err += our_error(weights, lambda1, lambda2)
 
-    return our_error(y[:, [i]], x2, weights, lambda1, lambda2)
+
+    return err
 
         
 
@@ -132,19 +141,32 @@ def train_multilayer_network(X, Y, weight_updating_function=update_weights, num_
     
     #First stage of training
     while reasonable_lambda:
-        while error_diff > 0:
+        while error_diff > 0.001:
             prev_error = cur_error
             cur_error = weight_updating_function(X, Y, weights, bias, eta, T, lambda1, lambda2)
             error_diff = prev_error - cur_error
-            print(error_diff)
-        error_diff = 1
         lambda1 *= 10
         T += 1
-        print("p" + str(prev_error))
-        print("c" + str(cur_error))
+        error_diff = 1
+        # Do one check outside loop to make sure jump is not too high
+        cur_error = weight_updating_function(X, Y, weights, bias, eta, T, lambda1, lambda2)
         if prev_error*5 <= cur_error:
             reasonable_lambda = False
+
+    print("done with first stage")
+    #Second stage of training
+    #get error back to previous ammount
+    min_error = prev_error
+    while cur_error > min_error:
+        while error_diff > 0.001:
+            prev_error = cur_error
+            cur_error = weight_updating_function(X, Y, weights, bias, eta, T, lambda1, lambda2)
+            error_diff = prev_error - cur_error
+        lambda1 /= 2.0
+        error_diff = 1
+
     return weights, bias
+
 
 def predict_multilayer_network(X, weights, bias, hidden_layer_fn, output_layer_fn, T):
     """Fully propagate inputs through the entire neural network,
